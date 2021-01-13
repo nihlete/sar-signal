ef67cdaa0d17be5578dbea41ff7de9ad2d48e304
nihlet <v.andrei@protonmail.com>
2021-01-14 00:28:40 +0300

    charts

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,26 @@ import Linear.Metric
 import Linear.V3
 import Linear.Vector
 import MLS (mls)
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
 
 j :: RealFloat a => Complex a
 j = 0 :+ 1
 
 main :: IO ()
-main = print code
+main = 
+  -- do 
+  -- print "IDDQD"
+  -- print code
+  toFile def "example1_big.png" $ do
+    layout_title .= "Amplitude Modulation"
+    setColors [opaque blue, opaque red]
+    plot (line "am" [signal' [0,(0.5)..400]])
+    plot (points "am points" (signal' [0,7..400]))
+
+
+signal' :: [Double] -> [(Double,Double)]
+signal' xs = [ (x,(sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- xs ]
 
 code = mls [True, False, True, False, False, True, False, False, True, False, False, True] [True, False, False, False, False, False, False, False, False, False, False]
 
